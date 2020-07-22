5b81360316aa0e250aa9f4f1939b015ec9a54c89
Santosh D <desirajusantosh@gmail.com>
2020-07-21 17:59:03 -0700

    saving of model for use in production

---
+++ prod_setup.py
@@ -0,0 +1,13 @@
+import pickle
+
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0]
+
+list(X_test.iloc[1,:])
